f23d855c593efe145f973d8ae6b4c345c32cab3e
rhk08 <140364137+rhk08@users.noreply.github.com>
2025-01-25 15:10:08 +1030

    css, js and html

---
+++ static/scripts.js
@@ -0,0 +1,33 @@
+document.querySelector('.show-tags').addEventListener('click', function() {
+    const tagContainer = document.querySelector('.tag-container');
+
+    const button = document.querySelector('.show-tags');
+    const blur = document.querySelector('.blur-background');
+    button.classList.add('fade-out');
+    blur.classList.add('fade-out');
+
+    const topics = ["Topic 1", "Topic 2", "Topic 3", "Topic 4"];
+
+    setTimeout(function() {
+        tagContainer.innerHTML = '';
+
+        topics.forEach(function(topic, index) {
+            const topicElement = document.createElement('a');
+            topicElement.classList.add('topic');
+            topicElement.textContent = topic;
+            tagContainer.appendChild(topicElement);
+
+            setTimeout(function() {
+                topicElement.classList.add('fade-in');
+            }, 200 * index);
+        });
+
+    }, 700);
+
+    
+});
+
+
+function populateTags(array){
+
+}
